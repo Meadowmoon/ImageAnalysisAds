3031d5da3d6fe1c347d15a7225fe584b475fd651
Leon <leonrl1984@gmail.com>
2018-08-09 00:27:29 +0800

    Adjust Web solution structure

---
+++ ImageAnalysisAds.Web/Readme.txt
@@ -0,0 +1,6 @@
+1. Install required Python packages:
+From command line prompt navigate to current repository and run the command: 
+> pip install -r requirements.txt
+
+2. Run the application:
+> python app.py
--- ImageAnalysisAds.Web/app.py
+++ ImageAnalysisAds.Web/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +9,15 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    name = request.form['inputName']
+    email = request.form['inputEmail']
+    password = request.form['inputPassword']
+
+    print(f'User name is {name}, email is {email}, password is {password}')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
