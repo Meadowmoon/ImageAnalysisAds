024403d10b007678c22a30723e196ce81261ddfe
LeonLiu <leonrl1984@gmail.com>
2018-08-10 16:21:39 +0800

    Add logger

--- ImageAnalysisAds.Web/app.py
+++ ImageAnalysisAds.Web/app.py
@@ -1,8 +1,18 @@
+import flask_login
 from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager
+from app_logger import Logger
+
+logger = ''
+
+# Initialize session manager
+login_manager = LoginManager()
 
 app = Flask(__name__)
+login_manager.init_app(app)
 
-@app.route("/")
+@app.route('/')
+@app.route('/main')
 def main():
     return render_template('index.html')
 
@@ -20,4 +30,5 @@ def signup():
     return redirect('/')
 
 if __name__ == '__main__':
+    logger = Logger().get_logger(__name__)
     app.run()
---
+++ ImageAnalysisAds.Web/app_logger.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import os
+import datetime
+
+class Logger():
+    # Initialize logs
+    LOG_PATH = 'logs'
+    LOG_FILE = '{}.txt'.format(datetime.date.today().isoformat())
+
+    def config(self,name):
+        logger = logging.getLogger(name)
+        if os.path.exists(self.LOG_PATH):
+            pass
+        else:
+            os.mkdir(self.LOG_PATH)
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+        file_handler = logging.FileHandler("%s/%s" % (self.LOG_PATH, self.LOG_FILE))
+        file_handler.setFormatter(formatter) 
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.formatter = formatter 
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+    def get_logger(self,name):
+        return self.config(name)
