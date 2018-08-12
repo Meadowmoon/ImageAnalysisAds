5acb222cda06b7978d9fdc26c076753932b5ba68
huaipeng <zhang.huaipeng@gmail.com>
2018-08-12 10:08:11 +0800

    add imageoverlay class

---
+++ ImageAnalysisAds.Mapper/overlayImage.py
@@ -0,0 +1,126 @@
+import cv2
+import numpy as np
+
+class CORNER:
+    TOP_LEFT = 1
+    TOP_RIGHT = 2
+    BOTTOM_LEFT = 3
+    BOTTOM_RIGHT = 4
+
+class OverlayOp:
+    def __init__(self, display=False):
+        ## Define a point (posx, posy) on the source
+        self.posx = 0  
+        self.posy = 0  
+        ## Define blending coefficients S and D
+        self.S = (0.2, 0.2, 0.2)  
+        self.D = (0.8, 0.8, 0.8)      
+        self.display = display      
+
+    def RmoveBackground(self, image):
+        if(self.display):
+            cv2.imshow('Original', image)
+            cv2.waitKey(0)
+        copy = image.copy()
+         
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        if(self.display):
+            cv2.imshow('Gray', gray)
+            cv2.waitKey(0)
+         
+        edged = cv2.Canny(gray, 10, 250)
+        if(self.display):
+            cv2.imshow('Edged', edged)
+            cv2.waitKey(0)
+         
+        kernel = np.ones((3, 3), np.uint8)
+         
+        dilation = cv2.dilate(edged, kernel, iterations=1)
+        if(self.display):
+            cv2.imshow('Dilation', dilation)
+            cv2.waitKey(0)
+         
+        closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+        if(self.display):
+            cv2.imshow('Closing', closing)
+            cv2.waitKey(0)
+         
+        (image, cnts, hiers) = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+         
+        cont = cv2.drawContours(copy, cnts, -1, (0, 0, 0), 2, cv2.LINE_AA)
+        if(self.display):
+            cv2.imshow('Contours', cont)
+            cv2.waitKey(0)
+         
+        mask = np.zeros(cont.shape[:2], dtype="uint8") * 255
+         
+        # Draw the contours on the mask
+        cv2.drawContours(mask, cnts, -1, (255, 255, 255), -1)
+         
+        # remove the contours from the image and show the resulting images
+        img = cv2.bitwise_and(cont, cont, mask=mask)
+        if(self.display):
+            cv2.imshow("Mask", img)
+            cv2.waitKey(0)
+         
+        for c in cnts:
+            x, y, w, h = cv2.boundingRect(c)
+            if w > 50 and h > 50:
+                new_img = img[y:y + h, x:x + w]                     
+                if(self.display):
+                    pass
+                    #cv2.imshow("Cropped", new_img)
+                    #cv2.waitKey(0)
+        
+        return img
+    
+    def OverlayImage(self, src, overlay):
+        src_height, src_width = src.shape[:2]
+        overlay_height, overlay_width = overlay.shape[:2]
+        
+        for x in range(overlay_width):
+            if x + self.posx < src_width:
+                for y in range(overlay_height):    
+                    if y + self.posy < src_width:
+                        source = src[y + self.posy, x + self.posx]
+                        over = overlay[y, x]
+                        merger = source
+                        if(over[0] != 0 and over[1] != 0 and over[2] != 0):
+                            merger = (self.S * source + self.D * over)                                 
+                        src[y + self.posy, x + self.posx] = merger
+                        
+        if(self.display): 
+            cv2.imshow('image', src) 
+            cv2.waitKey(0) 
+        cv2.imwrite('result.png', src)  # Saves the image
+        
+        return src
+    
+    def Operator(self, src_file, overlay_file, ratio, corner):
+        # Load a source image
+        src = cv2.imread(src_file)  
+        # Load an image to overlay 
+        overlay = cv2.imread(overlay_file)  
+        # Resize overlay image.
+        overlay = cv2.resize(overlay, (0, 0), fx=ratio, fy=ratio) 
+        overlay = self.RmoveBackground(overlay)        
+        src_height, src_width = src.shape[:2]
+        overlay_height, overlay_width = overlay.shape[:2]
+        if corner == CORNER.TOP_LEFT :
+            self.posx = 0
+            self.posy = 0
+        elif corner == CORNER.TOP_RIGHT :
+            self.posx = src_width - overlay_width - 1
+            self.posy = 0
+        elif corner == CORNER.BOTTOM_LEFT :
+            self.posx = 0
+            self.posy = src_height - overlay_height - 1
+        elif corner == CORNER.BOTTOM_RIGHT :
+            self.posx = src_width - overlay_width - 1
+            self.posy = src_height - overlay_height - 1
+            
+        return self.OverlayImage(src, overlay)
+
+overlayOp = OverlayOp(False)     
+overlayOp.Operator("dog.jpg", "dog-food.jpg", 0.5, CORNER.BOTTOM_RIGHT)
+cv2.destroyAllWindows() 
