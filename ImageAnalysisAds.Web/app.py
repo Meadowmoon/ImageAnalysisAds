8dac51b1cb8e668f4d83b24f3e574b21d4f38e60
Leon <leonrl1984@gmail.com>
2018-08-13 23:20:05 +0800

    Setup database

--- ImageAnalysisAds.Web/app.py
+++ ImageAnalysisAds.Web/app.py
@@ -1,9 +1,12 @@
 import flask_login
+
 from flask import Flask, render_template, request, redirect
 from flask_login import LoginManager
 from app_logger import Logger
+from app_database import db_session, init_db
+from app_models import User
 
-logger = ''
+logger = None
 
 # Initialize session manager
 login_manager = LoginManager()
@@ -26,9 +29,18 @@ def signup():
     email = request.form['inputEmail']
     password = request.form['inputPassword']
 
+    user = User(name, email, password)
+    db_session.add(user)
+    db_session.commit()
+
     print(f'User name is {name}, email is {email}, password is {password}')
     return redirect('/')
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 if __name__ == '__main__':
     logger = Logger().get_logger(__name__)
+    init_db()
     app.run()
