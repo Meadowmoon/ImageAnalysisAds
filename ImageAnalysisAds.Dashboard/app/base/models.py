97ba813e9221f1907c103353a61c1d5e2617f0cb
Leon <leonrl1984@gmail.com>
2019-01-14 00:07:12 +0800

    Integrated with Kim's API

--- ImageAnalysisAds.Dashboard/app/base/models.py
+++ ImageAnalysisAds.Dashboard/app/base/models.py
@@ -1,7 +1,7 @@
 from app import db, login_manager
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import DateTime, Column, Integer, String, ForeignKey
+from flask_sqlalchemy import SQLAlchemy 
 
 class User(db.Model, UserMixin):
 
@@ -25,14 +25,65 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return str(self.username)
 
+class Image(db.Model):
+
+    __tablename__ = 'Image'
+
+    id = Column(Integer, primary_key=True)
+    path = Column(String(120), unique=False)
+    size = Column(String(30), nullable=False)
+    type = Column(String(10))
+
+    def __repr__(self):
+        return str(self.type)
+
+class UserImage(db.Model):
+
+    __tablename__ = 'UserImage'
+
+    id = Column(Integer, primary_key=True)
+    image_id = Column(Integer, db.ForeignKey('Image.id'), nullable=False)
+    user_id = Column(Integer, db.ForeignKey('User.id'), nullable=False)
+    upload_datatime = Column(db.DateTime)
+    device = Column(String(10))
+
+    def __repr__(self):
+        return str(self.id)
+
+class Mapping(db.Model):
+
+    __tablename__ = 'Mapping'
+
+    id = Column(Integer, primary_key=True)
+    object_type = Column(String(30), nullable=False)
+    frameimage_id = Column(Integer, db.ForeignKey('Image.id'), nullable=False)
+
+    def __repr__(self):
+        return str(self.object_type)
+
+class Activity(db.Model):
+
+    __tablename__ = 'Activity'
+
+    id = Column(Integer, primary_key=True)
+    userimage_id = Column(Integer, db.ForeignKey('UserImage.id'), nullable=False)
+    object_type = Column(String(30), nullable=False)
+    result_json = Column(String(1024))
+    frameimage_id = Column(Integer, db.ForeignKey('Image.id'), nullable=False)
+    processtime = Column(Integer)
+
+    def __repr__(self):
+        return str(self.id)
 
 @login_manager.user_loader
 def user_loader(id):
     return User.query.filter_by(id=id).first()
-
 
 @login_manager.request_loader
 def request_loader(request):
     username = request.form.get('username')
     user = User.query.filter_by(username=username).first()
     return user if user else None
+
+
+
--- ImageAnalysisAds.Dashboard/app/config.py
+++ ImageAnalysisAds.Dashboard/app/config.py
@@ -2,7 +2,6 @@ class Config(object):
     SECRET_KEY = 'key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
 class ProductionConfig(Config):
     DEBUG = False
 
